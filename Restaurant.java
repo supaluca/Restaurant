6a947d55abc93558341918c902d9f529c5f82243
supaluca <supaluca13@gmail.com>
2025-04-01 09:22:46 -0400

    asdsa

---
+++ Restaurant.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Restaurant{
+    private ArrayList<String> menu;
+    private String name;
+    private String location;
+    private String type;
+
+    public Restaurant(ArrayList<String> m, String n, String l, String t){
+        menu = m;
+        name = n;
+    }
+}
